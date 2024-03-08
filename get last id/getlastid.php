5a188054a635fbab2bfb994c665d419bcaafa9a3
mohamedmhand78 <147414996+mohamedmhand78@users.noreply.github.com>
2024-03-08 13:33:16 +0100

    Add files via upload

---
+++ get last id/getlastid.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <label for="">First Name</label>
+        <input type="text" name="firstname" required>
+        <br>
+        <br>
+        <label for="">Last Name</label>
+        <input type="text" name="lastname" required>
+        <br>
+        <br>
+        <label for="">Email</label>
+        <input type="text" name="email" required>
+        <br>
+        <br>
+        <input type="submit" name="submit" value="Get Last Id Inseted" class="sub">
+    </form>
+    <?php
+    if (isset($_POST["submit"])) {
+        @$firstname = $_POST["firstname"];
+        @$lastname = $_POST["lastname"];
+        @$email = $_POST["email"];
+        
+        $servername = 'mysql:host=localhost;dbname=test;';
+
+        $user = 'root';
+
+        $pass = '';
+
+        try {
+            $connect = new PDO($servername, $user, $pass);
+
+            $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $query = "INSERT INTO testtb1 (First_name,Last_name,Email) VALUES ('$firstname','$lastname','$email')";
+            $connect->exec($query);
+            $last_id = $connect->lastInsertId();
+            echo "<p id='green'>Last Id Inserted Is :</p><br>" ;
+            echo "<p id='green'>$last_id</p><br>" ;
+
+        } catch (PDOException $e) {
+            echo "<p id='red'>Geting Failed</p>" . $e->getMessage();
+            
+        }
+    }
+    $connect = null;
+    ?>
+</body>
+</html>
---
+++ get last id/test.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 05 mars 2024 à 15:49
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `testtb1`
+--
+
+CREATE TABLE `testtb1` (
+  `id` int(11) NOT NULL,
+  `First_name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `testtb1`
+--
+ALTER TABLE `testtb1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `testtb1`
+--
+ALTER TABLE `testtb1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
